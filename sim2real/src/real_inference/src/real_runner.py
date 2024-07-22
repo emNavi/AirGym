f1664680e0ea70bcd5fd5cd8bc887cc803282c20
KJaebye <kangyao.huang@outlook.com>
2024-07-22 14:00:09 +0800

    [add]roslaunch for real_inference

--- sim2real/src/real_inference/src/real_runner.py
+++ sim2real/src/real_inference/src/real_runner.py
@@ -3,12 +3,13 @@
 import numpy as np
 import os
 import yaml
+import argparse
 
 import sys
 script_dir = os.path.dirname(os.path.realpath(__file__))
 airgym_dir = os.path.abspath(os.path.join(script_dir, "../../.."))
 sys.path.insert(0, airgym_dir)
-yaml_path = os.path.join(script_dir, 'config.yml')
+yaml_path = os.path.join(script_dir, 'X152b_inference.yaml')
 
 from airgym.envs import *
 
@@ -39,7 +40,7 @@ def get_args():
         {"name": "--train", "required": False, "help": "train network", "action": 'store_true'},
         {"name": "--play", "required": False, "help": "play(test) network", "action": 'store_true'},
         {"name": "--checkpoint", "type": str, "required": False, "help": "path to checkpoint"},
-        {"name": "--file", "type": str, "default": "X152b_inference.yaml", "required": False, "help": "path to config"},
+        {"name": "--file", "type": str, "default": yaml_path, "required": False, "help": "path to config"},
         {"name": "--num_envs", "type": int, "default": "1", "help": "Number of environments to create. Overrides config file if provided."},
 
         {"name": "--sigma", "type": float, "required": False, "help": "sets new sigma value in case if 'fixed_sigma: True' in yaml config"},
@@ -67,6 +68,61 @@ def get_args():
     args.sim_device = args.sim_device_type
     if args.sim_device=='cuda':
         args.sim_device += f":{args.sim_device_id}"
+    return args
+
+def ros_get_args():
+    custom_parameters = [
+        {"name": "--seed", "type": int, "default": 0, "required": False, "help":  "Random seed, if larger than 0 will overwrite the value in yaml config."},
+        {"name": "--tf", "required": False, "help": "run tensorflow runner", "action": 'store_true'},
+        {"name": "--train", "required": False, "help": "train network", "action": 'store_true'},
+        {"name": "--play", "required": False, "help": "play(test) network", "action": 'store_true'},
+        {"name": "--checkpoint", "type": str, "required": False, "help": "path to checkpoint"},
+        {"name": "--file", "type": str, "default": yaml_path, "required": False, "help": "path to config"},
+        {"name": "--num_envs", "type": int, "default": "1", "help": "Number of environments to create. Overrides config file if provided."},
+        {"name": "--sigma", "type": float, "required": False, "help": "sets new sigma value in case if 'fixed_sigma: True' in yaml config"},
+        {"name": "--track",  "action": 'store_true', "help": "if toggled, this experiment will be tracked with Weights and Biases"},
+        {"name": "--wandb-project-name", "type": str, "default": "rl_games", "help": "the wandb's project name"},
+        {"name": "--wandb-entity", "type": str, "default": None, "help": "the entity (team) of wandb's project"},
+        {"name": "--task", "type": str, "default": None, "help": "Override task from config file if provided."},
+        {"name": "--experiment_name", "type": str,  "help": "Name of the experiment to run or load. Overrides config file if provided."},
+        {"name": "--headless", "action": "store_true", "default": False, "help": "Force display off at all times"},
+        {"name": "--horovod", "action": "store_true", "default": False, "help": "Use horovod for multi-gpu training"},
+        {"name": "--rl_device", "type": str, "default": "cpu", "help": 'Device used by the RL algorithm, (cpu, gpu, cuda:0, cuda:1 etc..)'},
+        {"name": "--ctl_mode", "required": True, "type": str, "help": 'Specify the control mode and the options are: pos, vel, atti, rate, prop'},
+    ]
+
+    # 从ROS参数服务器获取参数
+    ros_params = {}
+    for param in custom_parameters:
+        name = param["name"].lstrip('--')
+        default_value = param.get("default", None)
+        ros_params[name] = rospy.get_param(f'{name}', default_value)
+
+    # 构建命令行参数
+    args_list = []
+    for param in custom_parameters:
+        name = param["name"].lstrip('--')
+        if ros_params[name] is not None:
+            if isinstance(ros_params[name], bool):
+                if ros_params[name]:
+                    args_list.append(param["name"])
+            else:
+                args_list.extend([param["name"], str(ros_params[name])])
+
+    # 使用argparse解析参数
+    parser = argparse.ArgumentParser()
+    for param in custom_parameters:
+        kwargs = {k: v for k, v in param.items() if k != "name"}
+        parser.add_argument(param["name"], **kwargs)
+
+    args = parser.parse_args(args_list)
+
+    args.physics_engine = None
+    args.sim_device = 'cpu'
+    args.use_gpu = False
+    args.subscenes = 0
+    args.use_gpu_pipeline = False
+    args.num_threads = 1
     return args
 
 
@@ -103,10 +159,7 @@ class RealRunner(Runner):
         self.player_factory.register_builder('cpu_player', lambda **kwargs : CpuPlayerContinuous(**kwargs))
 
 
-if __name__ == '__main__':
-    print("Python version:", sys.version)
-    args = vars(get_args())
-
+def main(args):
     config_name = args['file']
     args['play'] = True
 
@@ -125,3 +178,11 @@ if __name__ == '__main__':
     print(config)
 
     real_runner.run(args)
+
+if __name__ == '__main__':
+    try:
+        args = vars(ros_get_args())
+        main(args)
+    except (SystemExit, ConnectionRefusedError):
+        args = vars(get_args())
+        main(args)
