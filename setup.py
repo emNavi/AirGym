c33cf6d6a4f224baf169c672e6967bc3f4d589dc
kjaebye <kangyao.huang@outlook.com>
2025-04-01 16:33:19 +0800

    [update] readme

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='airgym',
-    version='0.0.2',
+    version='0.1.0',
     author='emNavi Tech',
     license="BSD 3-Clause",
     packages=find_packages(),
